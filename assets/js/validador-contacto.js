1e21fd7943f1bce0c2ca6d02b849c909424ace8f
javimun <pachu_javi@hotmail.com>
2022-10-18 17:14:35 -0300

    correcciones de algunas paginas y su responsive

--- assets/js/validador-contacto.js
+++ assets/js/validador-contacto.js
@@ -4,7 +4,42 @@ const SERVICIO = document.getElementById('servicio');
 const CONSULTA = document.getElementById('consulta');
 const FORM = document.querySelector('form');
 
-function validarDatos(e){
+function validarNombre() {
+    let regExpNombre = new RegExp("[A-Za-z\s]");
+    if (regExpNombre.test(NOMBRE.value) && NOMBRE.value.length >= 5) {
+        return true;
+    } else {
+        return false;
+    }
+}
+NOMBRE.addEventListener('keyup', validarNombre);
+function validarEmail(){
+    /*let regExpEmail = new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$");
+    if(regExpEmail.test(EMAIL.value)){
+        console.log(true);
+        return true;
+    }else{
+        console.log(true);
+        return false;
+    }*/
+    /*API PARA LA VALIDACIÓN DE DIRECCIONES DE EMAIL
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", "QwImUtMQjTDsYbAnznSCcphpuasVY0YT");
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
+    //FALTA AGREGAR LA VARIABLE MAIL PARA QUE FUNCIONE
+    fetch("https://api.apilayer.com/email_verification/check?email=email", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    */
+}
+
+function enviarDatos(e){
     e.preventDefault()
     let form = {
         name: NOMBRE.value,
@@ -16,6 +51,6 @@ function validarDatos(e){
     FORM.reset();
 }
 
-FORM.addEventListener('submit', validarDatos);
+FORM.addEventListener('submit', enviarDatos);
 
 
